f124340c844b41e2cb7e90fee02282e8e090cad5
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-21 17:20:48 +0200

    First c++ string I/O program

---
+++ basics/string_io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout << "Enter your name: ";
+    cin >> str ;
+    cout << "Welcome to C++, " << str << endl;
+}
