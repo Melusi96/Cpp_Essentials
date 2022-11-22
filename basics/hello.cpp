5f2deec9df21eca7424a16b3140dd69bc3480d8c
Melusi Gumbi <gmbmel003@myuct.ac.za>
2022-11-22 04:42:14 +0200

    Created a hello program
    Created a simple hello program and modified the string program just to add comments

---
+++ basics/hello.cpp
@@ -0,0 +1,15 @@
+/*
+Name of the program: Hello program
+Author's Name: Melusi Gumbi
+Date: 22 November 2022
+Brief Description: Simple hello program. Prints out hello to the user
+*/
+
+//----- hello.cpp ----------------------------------
+#include <iostream>
+using namespace std; // brings whole std namespace into scope
+int main(){
+    cout << "Hello from C++.\n";
+    cout << "Hello from C++." << endl;
+    return 0;
+}
--- basics/string_io.cpp
+++ basics/string_io.cpp
@@ -1,3 +1,13 @@
+/*
+Name of the program: String Input/Output
+Author's Name: Melusi Gumbi
+Date: 21 November 2022
+Brief Description: Inform the user that they should enter their name,
+    take in the user's name and print it out to the screen with a
+    sentence containing the user's name.
+*/
+
+//-----------string_io.cpp-------------
 #include <iostream>
 #include <string>
 
